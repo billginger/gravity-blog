6e89284025a8bc91402f56e7b1467c7fc1ecc1ff
Bill <jfg@163.com>
2020-12-21 21:33:15 +0800

    Add mongoose

--- apps/earth/src/app.ts
+++ apps/earth/src/app.ts
@@ -1,9 +1,17 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
 import config from './config';
 import routes from './routes';
 
+// Create Express server
 const app = express();
+
+// Connect to MongoDB
+mongoose.connect(config.mongodb_uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => console.log('MongoDB connection succeeded!'));
 
 // Static files path
 const staticPath = path.join(__dirname, '../static');
@@ -21,5 +29,6 @@ app.use(express.static(staticPath));
 // Routes setting
 app.get('*', routes);
 
+// Start Express server
 const listenLog = `App is running at http://localhost:${config.port}`;
 app.listen(config.port, () => console.log(listenLog));
--- apps/earth/src/config.example.ts
+++ apps/earth/src/config.example.ts
@@ -1,4 +1,6 @@
 export default {
   // App listen port
   port: 3000,
+  // MongoDB URI
+  mongodb_uri: 'mongodb://localhost/test',
 }
