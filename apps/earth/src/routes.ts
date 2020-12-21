5451eb8c8db9867fc49d3b5a6b085c0eb703e481
Bill <jfg@163.com>
2020-12-21 23:45:51 +0800

    Add url match logic

--- apps/earth/src/routes.ts
+++ apps/earth/src/routes.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from 'express';
+import Page from './models/page';
 
 const routes = (req: Request, res: Response) => {
   const url = req.path;
-  res.send('Hello Gravity Blog!');
+  Page.findOne({ url }, (err, page) => {
+    if (err) return res.status(404).end();
+    res.send('Hello Gravity Blog!');
+  });
 };
 
 export default routes;
