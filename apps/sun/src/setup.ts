3180642853647fce068758b0dab88ae0473848f2
Bill <jfg@163.com>
2020-12-24 21:07:56 +0800

    Improve setup function

--- apps/sun/src/setup.ts
+++ apps/sun/src/setup.ts
@@ -1,12 +1,23 @@
+import crypto from 'crypto';
 import mongoose from 'mongoose';
 import config from './config';
+import { encryptPwd } from './utils/encrypt';
+import { Setting } from './models/setting';
+import { User } from './models/user';
 import { Page } from './models/page';
 
-// Connect to MongoDB
-mongoose.connect(config.mongodb_uri, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => console.log('MongoDB connection succeeded!'));
+// Setting document
+const secret = crypto.randomBytes(16).toString('hex');
+const setting = {
+  secret
+};
+
+// User document
+const password = encryptPwd('admin', secret);
+const user = {
+  name: 'admin',
+  password
+};
 
 // Page documents
 const pages = [
@@ -22,12 +33,37 @@ const pages = [
   }
 ];
 
-// Check and insert documents
-Page.countDocuments({}, (err, count) => {
-  if (err) return console.error('MongoDB countDocuments error!', err);
-  if (!!count) return console.log('MongoDB documents already exist!');
-  Page.insertMany(pages, err => {
-    if (err) return console.error('MongoDB insertMany error!', err);
-    console.log('Setup successfully!');
-  });
+// Connect to MongoDB
+console.log('Connecting to MongoDB...');
+mongoose.connect(config.mongodb_uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', async () => {
+  console.log('MongoDB connection succeeded!');
+  console.log('Checking collections...');
+  try {
+    const countSetting = await Setting.countDocuments({});
+    const countUser = await User.countDocuments({});
+    const countPage = await Page.countDocuments({});
+    if (!!countSetting || !!countUser || !!countPage) {
+      console.warn('MongoDB collections already exist!');
+      process.exit();
+    }
+  } catch (err) {
+    console.error('MongoDB check collections error!', err);
+    process.exit();
+  }
+  console.log('Check collections succeeded!');
+  console.log('Creating collections...');
+  try {
+    await Setting.insertMany(setting);
+    await User.insertMany(user);
+    await Page.insertMany(pages);
+  } catch (err) {
+    console.error('MongoDB create collections error!', err);
+    process.exit();
+  }
+  console.log('Create collections succeeded!');
+  console.log('Congratulations! Setup successfully!');
+  process.exit();
 });
