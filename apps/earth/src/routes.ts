d6ec58afbafbba20ccd63efd1e2cd64c2ad0485b
Bill <jfg@163.com>
2021-01-01 22:09:33 +0800

    Improve routes

--- apps/earth/src/routes.ts
+++ apps/earth/src/routes.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
-import { PageType, Page } from './models/page';
+import { Page } from './models/page';
 
 const routes = (req: Request, res: Response) => {
   const url = req.path;
-  Page.findOne({ url }, (err, page: PageType) => {
+  Page.findOne({ url }, (err, page) => {
     if (err) return console.error('MongoDB findOne error!', err);
     if (!page) return res.status(404).end();
     const { title, content } = page;
